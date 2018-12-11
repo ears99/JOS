e9da2a5b60920442c724614729511aa6306c6d2e
Jacob S <bloggingpenguin@gmail.com>
2018-12-11 13:30:18 -0500

    Updated read_disk.asm

--- read_disk.asm
+++ read_disk.asm
@@ -4,9 +4,8 @@ read_disk:
 	mov dl, 0x80 ;boot from HDD = 0x80. 0x00 = RM (removable media).
 	mov cl, 0 ;select first cylinder
 	mov dh, 0 ;first head
-	;NOTE: mov al and cl are put into main.asm
-	;mov al, 1 ;only read one sector
-	;mov cl, 2 ;start reading second sector
+	mov al, 1 ;only read one sector
+	mov cl, 2 ;start reading second sector
 
 	;load 512 bytes after 0x7C00
 	;we can't set the segment registers directly.
@@ -16,14 +15,14 @@ read_disk:
 	mov bx, 0
 	mov es, bx
 	pop bx
-	mov bx, 0x7C00 + 512 ;load us 512 bytes after 0x7C00
+	mov bx, 0x7C00 + 512 ;load us 512 bytes after 0x7C00, 0x7E00.
 
-	;int 0x13 ;BIOS int for reading disk
+	int 0x13 ;BIOS int for reading disk
 
-	;jc disk_error ;if CF is set, jump to disk_error
-	;popa ;else pop all registers off the stack
-	;ret ;exit read_disk subroutine
+	jc disk_error ;if CF is set, jump to disk_error
+	popa ;else pop all registers off the stack
+	ret ;exit read_disk subroutine
 disk_error:
-	;mov si, DISK_ERR_MSG ;move DISK_ERR_MSG into si
-	;call print_str ;call the print_str subroutine
-	;jmp $ ;hang at current memory location
+	mov si, DISK_ERR_MSG
+	call print_str
+	jmp $ ;hang at current memory location
